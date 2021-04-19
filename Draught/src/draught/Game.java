ef456bc65cfb745fc00fb21924d182a490f4c8c1
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-04-19 15:03:30 +0200

    initialize board

--- Draught/src/draught/Game.java
+++ Draught/src/draught/Game.java
@@ -2,10 +2,20 @@ package draught;
 
 public class Game {
 
-    private int size;
-
-    public Game(int size) {
-        this.size = size;
-
+    private int n;
+    private Pawn[][] fields;
+    public Game(int n) {
+        this.n = n;
+        this.fields = new Pawn[n][n];
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields[0].length; j++) {
+                if  ((i+j) % 2 != 0){
+                    fields[i][j] = new Pawn();
+                }
+                else{
+                    fields[i][j] = null;
+                }
+            }
+        }
     }
 }
