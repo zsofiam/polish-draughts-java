a13d7fe3d6e14d412682939704891dc6681ad320
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-04-19 15:06:36 +0200

    ask for user input at generating board

--- Draught/src/draught/Game.java
+++ Draught/src/draught/Game.java
@@ -1,11 +1,16 @@
 package draught;
 
+import java.util.Scanner;
+
 public class Game {
 
     private int n;
     private Pawn[][] fields;
-    public Game(int n) {
-        this.n = n;
+    private Scanner scanner;
+    public Game() {
+        scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int n = Integer.parseInt(input);
         this.fields = new Pawn[n][n];
         for (int i = 0; i < fields.length; i++) {
             for (int j = 0; j < fields[0].length; j++) {
